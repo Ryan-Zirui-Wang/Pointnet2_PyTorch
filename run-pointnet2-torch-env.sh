bd424b86c96c9519d1eb103991893c210322b3b3
zirui-ryan-wang <wang_zr@live.com>
2019-04-21 16:09:33 +0100

    update docker related bash script

--- run-pointnet2-torch-env.sh
+++ run-pointnet2-torch-env.sh
@@ -1,1 +1,19 @@
-docker run -it --rm --runtime=nvidia -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch ziruiryanwang/pointnet2-torch
+docker run -it --rm --runtime=nvidia \
+    -p 8888:8888 \
+    -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
+    -v /media/ryan/hdd:/media/ryan/hdd \
+    -v /home/ryan/Desktop/PointClouds:/home/ryan/Desktop/PointClouds \
+    ziruiryanwang/pointnet2-torch
+
+
+# docker run -it --rm --runtime=nvidia \
+#     -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
+#     -v /media/ryan/hdd:/media/ryan/hdd \
+#     ziruiryanwang/pointnet2-torch
+
+
+# docker run -it --rm --runtime=nvidia \
+#     -p 8888:8888 \
+#     -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
+#     -v /media/ryan/hdd:/media/ryan/hdd \
+#     ziruiryanwang/pointnet2-torch:my-etw
