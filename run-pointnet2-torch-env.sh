23c260b3d96fc5f822f87ff829d00e4fafd8a4fb
zirui-ryan-wang <wang_zr@live.com>
2019-04-22 17:24:44 +0100

    change related notebook functions calls

--- run-pointnet2-torch-env.sh
+++ run-pointnet2-torch-env.sh
@@ -1,17 +1,18 @@
-docker run -it --rm --runtime=nvidia \
-    -p 8888:8888 \
-    -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
-    -v /media/ryan/hdd:/media/ryan/hdd \
-    -v /home/ryan/Desktop/PointClouds:/home/ryan/Desktop/PointClouds \
-    ziruiryanwang/pointnet2-torch
-
-
+# Jupyter notebook
 # docker run -it --rm --runtime=nvidia \
+#     -p 8888:8888 \
 #     -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
 #     -v /media/ryan/hdd:/media/ryan/hdd \
+#     -v /home/ryan/Desktop/PointClouds:/home/ryan/Desktop/PointClouds \
 #     ziruiryanwang/pointnet2-torch
 
+# Normal training
+docker run -it --rm --runtime=nvidia \
+    -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
+    -v /media/ryan/hdd:/media/ryan/hdd \
+    ziruiryanwang/pointnet2-torch
 
+# My etw tool repo
 # docker run -it --rm --runtime=nvidia \
 #     -p 8888:8888 \
 #     -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
