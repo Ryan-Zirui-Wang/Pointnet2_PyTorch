6404751dc51d664b674812c5de601e88f37aa555
zirui-ryan-wang <wang_zr@live.com>
2019-04-22 19:09:05 +0100

    can use py file to compute vector field now

--- run-pointnet2-torch-env.sh
+++ run-pointnet2-torch-env.sh
@@ -10,6 +10,7 @@
 docker run -it --rm --runtime=nvidia \
     -v /home/ryan/repos-developing/Pointnet2_PyTorch:/home/ryan/repos-developing/Pointnet2_PyTorch \
     -v /media/ryan/hdd:/media/ryan/hdd \
+    -v /home/ryan/Desktop/PointClouds:/home/ryan/Desktop/PointClouds \
     ziruiryanwang/pointnet2-torch
 
 # My etw tool repo
